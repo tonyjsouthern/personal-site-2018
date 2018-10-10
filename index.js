7453ac7777a7a2f16d3668b88c51c9fc18690d22
TJ <tonyjsouthern@gmail.com>
2018-10-10 16:15:20 -0700

    intial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var axios = require("axios");
+var express = require("express");
+var path = require('path');
+const app = express();
+
+// Host the following directory to access application
+app.use(express.static(path.join(__dirname, '/public')));
+
+function wakeUp () {
+  axios.get('http://localhost:8000/wake').then (() => {
+    console.log("Requesting wake up...")
+  })
+}
+
+setTimeout(wakeUp, 1740000)
+
+app.get('/wake', function(res){
+  console.log("Rise And Shine!")
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
+ });
